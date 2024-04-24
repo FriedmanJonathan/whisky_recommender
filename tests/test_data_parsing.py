edff334fe12ca5186cf5e55de4457d017df99d14
Jonathan Friedman <yonifriedman10@gmail.com>
2024-04-24 14:30:47 +0300

    feat: added parametrization to tests so all CSVs can be checked at once

--- tests/test_data_parsing.py
+++ tests/test_data_parsing.py
@@ -2,31 +2,31 @@ import pandas as pd
 import pytest
 import os
 
+CSV_DIRECTORY = './'
+
+
+@pytest.fixture(params=[file for file in os.listdir(CSV_DIRECTORY) if file.endswith('.csv')])
+def csv_file(request):
+    return request.param
+
+
+@pytest.fixture
+def expected_columns(csv_file):
+    # Define your expected columns mappings here
+    expected_columns_map = {
+        'whisky_details_100.csv': ['whisky_url', 'distillery_name_inner', 'country', 'region', 'whisky_type', 'whisky_age_inner', 'alcohol_pct_inner', 'bottler', 'post_treatment', 'nosing_notes', 'tasting_notes', 'finish_notes']
+        # Add more mappings as needed - this is TODO, currently test is working ok but failing on other CSVs since we didnt' specify column names
+    }
+    return expected_columns_map.get(csv_file, [])
+
+
+@pytest.fixture
+def df(csv_file):
+    csv_filepath = os.path.join(CSV_DIRECTORY, csv_file)
+    return pd.read_csv(csv_filepath)
+
+
 class TestDataColumns:
-    CSV_DIRECTORY = 'csv_files/'
 
-    def __init__(self, csv_filename):
-        self.csv_filename = csv_filename
-
-    @property
-    def csv_filepath(self):
-        return os.path.join(self.CSV_DIRECTORY, self.csv_filename)
-
-    @pytest.fixture
-    def df(self):
-        return pd.read_csv(self.csv_filepath)
-
-    def test_loaded_data_has_correct_columns(self, df):
-        expected_columns_map = {
-            'whisky_details_100.csv': ['whisky_url', 'distillery_name_inner', 'country', 'region', 'whisky_type', 'whisky_age_inner', 'alcohol_pct_inner', 'bottler', 'post_treatment', 'nosing_notes', 'tasting_notes', 'finish_notes'],
-            # Add more mappings as needed
-        }
-        expected_columns = expected_columns_map.get(self.csv_filename, [])
-
+    def test_loaded_data_has_correct_columns(self, df, expected_columns):
         assert df.columns.to_list() == expected_columns
-
-def test_data_columns():
-    csv_directory = 'csv_files/'
-    for csv_file in [file for file in os.listdir(csv_directory) if file.endswith('.csv')]:
-        test = TestDataColumns(csv_file)
-        test.test_loaded_data_has_correct_columns()

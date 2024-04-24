b9c483100daeb2acd36cbbbb57825891184bf0a3
Jonathan Friedman <yonifriedman10@gmail.com>
2024-04-24 14:37:41 +0300

    chore: added comments and linting

--- tests/test_data_parsing.py
+++ tests/test_data_parsing.py
@@ -7,21 +7,53 @@ CSV_DIRECTORY = './'
 
 @pytest.fixture(params=[file for file in os.listdir(CSV_DIRECTORY) if file.endswith('.csv')])
 def csv_file(request):
+    """
+    Fixture to provide CSV file names found in the specified directory.
+
+    Parameters:
+        request: pytest request object
+
+    Returns:
+        str: Name of a CSV file
+    """
     return request.param
 
 
 @pytest.fixture
 def expected_columns(csv_file):
+    """
+    Fixture to provide the expected column names for a given CSV file.
+
+    Parameters:
+        csv_file (str): Name of the CSV file
+
+    Returns:
+        list: Expected column names for the CSV file
+    """
     # Define your expected columns mappings here
     expected_columns_map = {
-        'whisky_details_100.csv': ['whisky_url', 'distillery_name_inner', 'country', 'region', 'whisky_type', 'whisky_age_inner', 'alcohol_pct_inner', 'bottler', 'post_treatment', 'nosing_notes', 'tasting_notes', 'finish_notes']
-        # Add more mappings as needed - this is TODO, currently test is working ok but failing on other CSVs since we didnt' specify column names
+        'whisky_details_100.csv': [
+            'whisky_url', 'distillery_name_inner', 'country', 'region',
+            'whisky_type', 'whisky_age_inner', 'alcohol_pct_inner',
+            'bottler', 'post_treatment', 'nosing_notes', 'tasting_notes',
+            'finish_notes'
+        ]
+        # Add more mappings as needed - this is TODO
     }
     return expected_columns_map.get(csv_file, [])
 
 
 @pytest.fixture
 def df(csv_file):
+    """
+    Fixture to load a CSV file into a pandas DataFrame.
+
+    Parameters:
+        csv_file (str): Name of the CSV file
+
+    Returns:
+        pandas.DataFrame: Loaded DataFrame
+    """
     csv_filepath = os.path.join(CSV_DIRECTORY, csv_file)
     return pd.read_csv(csv_filepath)
 
@@ -29,4 +61,11 @@ def df(csv_file):
 class TestDataColumns:
 
     def test_loaded_data_has_correct_columns(self, df, expected_columns):
+        """
+        Test to verify that loaded data has correct columns.
+
+        Parameters:
+            df (pandas.DataFrame): Loaded DataFrame
+            expected_columns (list): Expected column names
+        """
         assert df.columns.to_list() == expected_columns

bf1f2de96a72c7d425dec01b1ede9407dbf1d4f1
Jonathan Friedman <yonifriedman10@gmail.com>
2024-05-28 15:17:38 +0300

    feat: added env file for environment variables, now need to test locally and then edit S3 implementation

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
-import csv
-import pandas as pd
+import re
 import sys
 import os
 from flask import Flask, request, jsonify, send_from_directory
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Add the parent directory to sys.path to access the scripts package
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
@@ -11,11 +14,14 @@ from scripts.modeling.whisky_recommender_model import recommend_whisky
 
 application = Flask(__name__, static_url_path='', static_folder='./frontend')
 
-# Define the CSV file where feedback will be saved and load feature store data
-csv_filename = 'distillery_data.csv'
-FEATURE_STORE_PATH = os.path.join('', 'data', 'processed', '2023_09', 'whisky_features_100.csv')
-FEEDBACK_DIR = os.path.join('', 'data', 'feedback', '2024_05')
-os.makedirs(FEEDBACK_DIR, exist_ok=True)
+# Environment setup
+IS_LOCAL = os.getenv('IS_LOCAL', 'True') == 'True'
+S3_BUCKET = os.getenv('S3_BUCKET')  # Only necessary when not local
+
+# Define the directories and files based on whether the app is running locally or on AWS
+if IS_LOCAL:
+    FEEDBACK_DIR = os.path.join('', 'data', 'feedback', '2024_05')
+    os.makedirs(FEEDBACK_DIR, exist_ok=True)
 
 # Main page
 @application.route('/')
@@ -28,62 +34,57 @@ def index():
 def submit_feedback():
     data = request.get_json()
     required_fields = ['whisky1', 'whisky2', 'whisky3', 'recommendedWhisky', 'feedback1', 'timestamp']
+    optional_fields = ['rating', 'feedback2', 'experience']
 
-    # Check if all required fields are provided
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing data'}), 400
 
     try:
-        # Extract feedback data
-        print(data)  # Log the incoming data
-        whisky1 = data['whisky1']
-        whisky2 = data['whisky2']
-        whisky3 = data['whisky3']
-        recommended_whisky = data['recommendedWhisky']
-        feedback1 = data['feedback1']
-        rating = data.get('rating')  # This field is optional
-        feedback2 = data.get('feedback2', '')  # This field is optional
-        experience = data['experience']  # New field for whisky experience
-        timestamp = data['timestamp']
+        # Log incoming data
+        print(data)
 
-        # Sanitize timestamp for use in filenames
-        sanitized_timestamp = timestamp.replace(':', '').replace('-', '').replace('.', '').replace('T', '_').replace('Z', '')
+        # Prepare feedback data for CSV
+        header = required_fields + optional_fields
+        feedback_entries = [data.get(field, '') for field in header]
 
-        # Generate a unique filename based on sanitized timestamp
-        feedback_file = os.path.join(FEEDBACK_DIR, f'feedback_{sanitized_timestamp}.csv')
-        print(f"Writing to file: {feedback_file}")  # Log the feedback file path
+        # Construct CSV content
+        csv_content = ','.join(['"{}"'.format(entry) for entry in header]) + '\n'  # Header row
+        csv_content += ','.join(['"{}"'.format(entry) for entry in feedback_entries])  # Data row
 
-        # Save feedback data to CSV
-        with open(feedback_file, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['whisky1', 'whisky2', 'whisky3', 'recommendedWhisky', 'feedback1', 'rating', 'feedback2', 'experience', 'timestamp'])
-            writer.writerow([whisky1, whisky2, whisky3, recommended_whisky, feedback1, rating, feedback2, experience, timestamp])
+        # Sanitize timestamp and create a filename
+        sanitized_timestamp = re.sub(r'[^a-zA-Z0-9]', '_', data['timestamp'])
+        feedback_file_name = f'feedback_{sanitized_timestamp}.csv'
+
+        # Decide storage based on environment
+        if IS_LOCAL:
+            feedback_file = os.path.join(FEEDBACK_DIR, feedback_file_name)
+            with open(feedback_file, 'w', newline='') as file:
+                file.write(csv_content)
+            print(f"Feedback written to local file: {feedback_file}")
+        else:
+            import boto3
+            s3 = boto3.client('s3')
+            s3.put_object(Bucket=S3_BUCKET, Key=feedback_file_name, Body=csv_content.encode('utf-8'))
+            print(f"Feedback written to S3 bucket {S3_BUCKET}")
 
         return jsonify({'message': 'Feedback submitted successfully'}), 200
     except Exception as e:
-        print(f"Error occurred: {e}")  # Log the error
+        print(f"Error occurred: {e}")
         return jsonify({'error': str(e)}), 500
 
-
+# Recommendation endpoint
 @application.route('/recommend', methods=['POST'])
 def recommend_whisky_endpoint():
     data = request.get_json()
-
     if 'whisky_names' not in data or not isinstance(data['whisky_names'], list):
         return jsonify({'error': 'Invalid input, list of whisky names expected'}), 400
 
     try:
-        whisky_names = data['whisky_names']
-
-        recommended_whisky_info = recommend_whisky(FEATURE_STORE_PATH, whisky_names)
-
-        recommended_whisky = recommended_whisky_info["Recommended Whisky"]
-
+        recommended_whisky = recommend_whisky(data['whisky_names'])
         return jsonify({'recommended_whisky': recommended_whisky})
     except Exception as e:
-        print(f"Error occurred: {e}")  # Log the error
+        print(f"Error occurred: {e}")
         return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0', port=8000)
